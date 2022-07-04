4315c52e550a7e2962a0d54f587ab73c1e2af465
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-07-04 18:11:39 -0400

    Create number-of-substrings-with-all-zeroes.py

---
+++ two-pointers/number-of-substrings-with-all-zeroes.py
@@ -0,0 +1,16 @@
+class Solution:
+    """
+    @param str: the string
+    @return: the number of substrings 
+    """
+    def string_count(self, str: str) -> int:
+        j, ans = 1, 0
+        for i in range(len(str)):
+            if str[i] != "0":
+                continue
+            j = max(j, i + 1)
+            while j < len(str) and str[j] == "0":
+                j += 1
+            ans += j - i
+
+        return ans
