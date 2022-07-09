357412fdfbd05389f89f1d5d57a2e9fe053c3bef
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-07-09 18:40:57 -0400

    Create serialize-and-deserialize-binary-tree.py

---
+++ bfs/serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,35 @@
+def serialize(self,root):
+    if root is None:
+        return"{}"
+    queue=[root]
+    index=0
+    while index<len(queue):
+        if queue[index]is not None:
+           queue.append(queue[index].left)
+           queue.append(queue[index].right)
+        index+=1
+    while queue[-1]is None:
+        queue.pop()
+    return'{%s}'%','.join([str(node.val)if node is not None else '#' for node in queue])
+  
+ def deserialize(self,data):
+    data=data.strip('\n')
+    if data =='{}':
+        return None
+    vals=data[1:-1].split(',')
+    root=TreeNode(int(vals[0]))
+    queue=[root]
+    isLeft Child=True
+    index=0
+    for val in vals[1:]:
+        if val is not'#':
+            node=TreeNode(int(val))
+            if isLeft Child:
+                queue[index].left=node
+            else:
+                queue[index].right=node
+            queue.append(node)
+        if not isLeftChild:
+            index+=1
+        isLeftChild = not isLeftChild
+    return root
