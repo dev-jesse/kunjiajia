224854b9f5ea06199b80497409730aca84562a58
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-07-10 16:08:55 -0600

    Create preorder-morris.py

---
+++ morris/preorder-morris.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+    @param root: A Tree
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorderTraversal(self, root):
+        nums = []
+        cur = None
+        
+        while root:
+            if root.left:
+                cur = root.left
+                while cur.right and cur.right != root:
+                    cur = cur.right
+                if cur.right == root:
+                    cur.right = None
+                    root = root.right
+                else:
+                    nums.append(root.val)
+                    cur.right = root
+                    root = root.left
+            else:
+                nums.append(root.val)
+                root = root.right
+                
+        return nums
