afdb6e379813d5cb26950f95466f78708dad7afc
Jesse <83730324+dev-jesse@users.noreply.github.com>
2022-07-10 16:08:27 -0600

    Create inorder-morris.py

---
+++ morris/inorder-morris.py
@@ -0,0 +1,27 @@
+class Solution:
+    """
+    @param root: A Tree
+    @return: Inorder in ArrayList which contains node values.
+    """
+    def inorderTraversal(self, root):
+        nums = []
+        cur = None
+    
+        while root:
+            if root.left:
+                cur = root.left
+                while cur.right and cur.right != root:
+                    cur = cur.right
+                
+                if cur.right == root:
+                    nums.append(root.val)
+                    cur.right = None
+                    root = root.right
+                else:
+                    cur.right = root
+                    root = root.left
+            else:
+                nums.append(root.val)
+                root = root.right
+                
+        return nums
